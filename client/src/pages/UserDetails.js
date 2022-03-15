348210e38ac66eb970aa6f799986f4b245caf01a
AbhishekICS <iabhishek.ics@gmail.com>
2022-03-15 17:40:42 +0530

    Add Logger in console

--- client/src/pages/UserDetails.js
+++ client/src/pages/UserDetails.js
@@ -10,6 +10,7 @@ export const UserDetails = () => {
   const addNewMarksUrl = "http://localhost:2000/api/mark/new";
 
   const [user, setUser] = useState([]);
+  const [all, setAll] = useState();
   const [marks, setMarks] = useState([]);
   const [allSubject, setAllSubject] = useState();
   const [subject, setSubject] = useState("");
@@ -18,10 +19,11 @@ export const UserDetails = () => {
   const getData = async () => {
     const data = await getDatas(URL_One);
     const datata = await data.json();
-    console.log(datata)
-    setUser(datata);
+    console.log(datata);
+    setAll(datata);
   };
 
+  console.log("All", all);
   const getAllSubject = async () => {
     if (localStorage.getItem("Token")) {
       let token = localStorage.getItem("Token").replace(/["]+/g, "");
@@ -42,30 +44,18 @@ export const UserDetails = () => {
     }
   };
 
-  const getAllMarks = async () => {
-    if (localStorage.getItem("Token")) {
-      let token = localStorage.getItem("Token").replace(/["]+/g, "");
-      try {
-        // const res = await
-        const res = await fetch(marks_url, {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            token: `Bearer ${token}`,
-          },
-        });
-        const listItems = await res.json();
-        setMarks(listItems);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
+  
 
   const addNewMarks = async (e) => {
     e.preventDefault();
-    if(!subject) {alert("Please select subject"); return;}
-    if(!obtain) {alert("Please enter obtained mark"); return;}
+    if (!subject) {
+      alert("Please select subject");
+      return;
+    }
+    if (!obtain) {
+      alert("Please enter obtained mark");
+      return;
+    }
 
     let someSubject = marks.some((item) => item.subject._id === subject);
     if (!someSubject) {
@@ -82,7 +72,7 @@ export const UserDetails = () => {
       });
       res = await res.json();
       e.target.reset();
-      getAllMarks();
+      getData()
     } else {
       alert("This Subject has already mark");
     }
@@ -99,78 +89,86 @@ export const UserDetails = () => {
       },
     });
     res = await res.json();
-    getAllMarks();
+    getData()
   };
   useEffect(() => {
     getData();
-    getAllMarks();
     getAllSubject();
   }, []);
 
   return (
     <>
-      <div className="user-card">
-        <h1>Name : {user.username}</h1>
-        <h3>Email : {user.email}</h3>
-      </div>
-      <div className="add-marks">
-        <form onSubmit={addNewMarks}>
-          <label>
-            <select
-              onChange={(e) => setSubject(e.target.value)}
-              value={subject}
-            >
-              <option value="" key={"initial_value"}>
-                Select subject
-              </option>
-              {allSubject &&
-                allSubject.map((s) => {
-                  return (
-                    <option value={s._id} key={s._id}>
-                      {s.name}
-                    </option>
-                  );
-                })}
-            </select>
-          </label>
-          <label>
-            <input
-              type="text"
-              placeholder="Enter Obtain mark"
-              onChange={(e) => setObtain(e.target.value)}
-            />
-          </label>
-          <input type="submit" value="Add Marks" />
-        </form>
-      </div>
+      {all ? (
+        <>
+          <div className="user-card">
+            <h1>Name : {all.oneStudent.username}</h1>
+            <h3>Email : {all.oneStudent.email}</h3>
+          </div>
 
-      <table>
-        <tr>
-          <th>Subjects</th>
-          <th>Total Marks</th>
-          <th>Obtain Marks</th>
-          <th>%</th>
-          <th>Delete</th>
-        </tr>
-        {marks.map((mark) => {
-          return (
-            <tr key={mark._id}>
-              <td>{mark.subject.name}</td>
-              <td>100</td>
-              <td>{mark.obtain}</td>
-              <td>{mark.obtain}%</td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => deleteMarks(mark._id)}
+          <div className="add-marks">
+            <form onSubmit={addNewMarks}>
+              <label>
+                <select
+                  onChange={(e) => setSubject(e.target.value)}
+                  value={subject}
                 >
-                  Delete
-                </button>
-              </td>
+                  <option value="" key={"initial_value"}>
+                    Select Subject
+                  </option>
+                  {allSubject &&
+                    allSubject.map((s) => {
+                      return (
+                        <option value={s._id} key={s._id}>
+                          {s.name}
+                        </option>
+                      );
+                    })}
+                </select>
+              </label>
+              <label>
+                <input
+                  type="text"
+                  placeholder="Enter Obtain mark"
+                  onChange={(e) => setObtain(e.target.value)}
+                />
+              </label>
+              <input type="submit" value="Add Marks" />
+            </form>
+          </div>
+
+          <table>
+            <tr>
+              <th>Subjects</th>
+              <th>Total Marks</th>
+              <th>Obtain Marks</th>
+              <th>%</th>
+              <th>Delete</th>
             </tr>
-          );
-        })}
-      </table>
+            {all.marks.map((mark) => {
+              return (
+                <tr key={mark._id}>
+                  <td>{mark.subject.name}</td>
+                  <td>100</td>
+                  <td>{mark.obtain}</td>
+                  <td>{mark.obtain}%</td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => deleteMarks(mark._id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </>
+      ) : (
+        <>
+          <h2>Somethings happen wrong..</h2>
+        </>
+      )}
     </>
   );
 };
